16d6cf7e6c7b8b43c46bb0ae0aae6601a854f487
Jash <128972302+Jashbaua@users.noreply.github.com>
2024-09-09 23:32:23 +0530

    Add project adding functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./style.css"
+import * as projects from './projects.js'
+import * as ui from './ui.js'
+
+projects.create('Default Project')
+projects.selectProject('Default Project')
+ui.refreshProjects(projects.getProjects())
+
+function createProjectClicked(){
+    ui.showNewProjectModal()
+}
+function projectNameFilled(projectName){
+    projects.create(projectName)
+    ui.refreshProjects(projects.getProjects())
+}
+
+
+export{createProjectClicked,projectNameFilled}
