b93c43c5cc6cdae50c2c22847a6f27891baf76d5
Jash <128972302+Jashbaua@users.noreply.github.com>
2024-09-10 13:42:02 +0530

    Add edit project functionality

--- src/index.js
+++ src/index.js
@@ -4,15 +4,40 @@ import * as ui from './ui.js'
 
 projects.create('Default Project')
 projects.selectProject('Default Project')
-ui.refreshProjects(projects.getProjects())
+ui.refreshContent(projects.getProjects())
 
 function createProjectClicked(){
     ui.showNewProjectModal()
 }
 function projectNameFilled(projectName){
+    if(!projects.canCreate(projectName)){
+        ui.throwProjectCreationError()
+    }
+    else ui.setProjectInputValid()
+}
+function projectEdited(projectName){
+    if(!projects.canCreate(projectName)){
+        ui.throwProjectEditError()
+    }
+    else ui.setProjectEditValid()
+
+}
+function editedProjectSaved(projectName){
+    projects.editSelected(projectName)
+    ui.refreshContent(projects.getProjects())
+}
+function projectNameAdded(projectName){
     projects.create(projectName)
-    ui.refreshProjects(projects.getProjects())
+    ui.refreshContent(projects.getProjects())
+}
+function projectClicked(projectName){
+    if(projects.isSelected(projectName)){
+        ui.showEditProjectModal()
+        return
+    }
+    projects.selectProject(projectName)
+    ui.refreshContent(projects.getProjects())
 }
 
 
-export{createProjectClicked,projectNameFilled}
+export{createProjectClicked,projectNameFilled,projectNameAdded,projectClicked,projectEdited,editedProjectSaved}
