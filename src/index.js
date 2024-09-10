0f697aa82e08c220c20110ecfb666138f8ff90bd
Jash <128972302+Jashbaua@users.noreply.github.com>
2024-09-10 16:40:20 +0530

    Add task adding functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css"
 import * as projects from './projects.js'
 import * as ui from './ui.js'
+import { format } from "date-fns"
 
 projects.create('Default Project')
 projects.selectProject('Default Project')
@@ -42,6 +43,15 @@ function projectDeleteClicked(){
     projects.deleteProject()
     ui.refreshContent(projects.getProjects())
 }
+function createTaskClicked(){
+    ui.showCreateTaskModal()
+}
+function taskSaveClicked(name,description,due,priority){
+    let formattedDue = due?format(new Date(due), 'M/d/yyyy, h:mmaaa'):'';    
+    projects.saveTask(name,description,formattedDue,priority)
+    ui.refreshContent(projects.getProjects())
+}
+function taskDeleteClicked(){}
 
 
-export{projectDeleteClicked,createProjectClicked,projectNameFilled,projectNameAdded,projectClicked,projectEdited,editedProjectSaved}
+export{taskDeleteClicked,taskSaveClicked,createTaskClicked,projectDeleteClicked,createProjectClicked,projectNameFilled,projectNameAdded,projectClicked,projectEdited,editedProjectSaved}
